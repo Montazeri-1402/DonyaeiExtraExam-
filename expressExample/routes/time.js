492eee80a54be20c936cc611c78fd420e02c5132
Samarium Code <cactuspydon@gmail.com>
2024-01-09 18:56:54 +0330

    First Commit

---
+++ expressExample/routes/time.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+const Time = [{ projectId: 1, title: 'a', startTime: '12:00', endTime: '16:00', date: "1398/14/5" },
+{ projectId: 2, title: 'a', startTime: '12:00', endTime: '16:00', date: "1398/14/5" },
+{ projectId: 1, title: 'a', startTime: '12:00', endTime: '16:00', date: "1398/14/5" }
+    , { projectId: 3, title: 'a', startTime: '12:00', endTime: '16:00', date: "1398/14/5" }]
+
+router.get('/get', (req, res) => {
+    let projectId = +(req.query.projectid)
+    
+   
+
+    let timeproject=[]   
+        Time.map((element)=>{
+            if(element.projectId===projectId){
+                timeproject.push(element)
+            }
+        })
+
+        res.send(timeproject)
+});
+
+
+router.post('/',(req,res)=>{
+    Time.push(JSON.stringify(req.body))
+    res.sendStatus(200)
+})
+
+
+router.put('/update',(req,res)=>{
+    
+})
+
+
+module.exports = router;
